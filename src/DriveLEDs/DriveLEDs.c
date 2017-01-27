c3f75981df4d8533120ca76b89d7eb389a4c2fd0
Ronnie Nigash <nigash@uw.edu>
2017-01-27 12:44:10 -0800

    Update Create, Add helper functions

--- src/DriveLEDs/DriveLEDs.c
+++ src/DriveLEDs/DriveLEDs.c
@@ -1,8 +1,26 @@
 #include "DriveLEDs.h"
 
+static uint16_t * addressOfLEDs;
+
+static uint16_t LEDState; // records state of LEDs
+
+enum {
+	ALL_LEDS_ON = ~0,
+	ALL_LEDS_OFF = ~ALL_LEDS_ON,
+	FIRST_LED = 1,
+	LAST_LED = 50
+};
+
+static void writeToHardware(void)
+{
+	*addressOfLEDs = LEDState;
+}
+
 void DriveLEDs_Create(uint16_t * address)
 {
-	*address = 0;
+	addressOfLEDs = address;
+	LEDState = (uint16_t)ALL_LEDS_OFF;
+	writeToHardware();
 }
 
 void DriveLEDs_Destroy()
