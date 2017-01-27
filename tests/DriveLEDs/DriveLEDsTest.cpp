19e43d7a3580a76df3a6dbc342d35f3ba40cc0de
Ronnie Nigash <nigash@uw.edu>
2017-01-27 12:19:20 -0800

    Add test for LEDs off after init

--- tests/DriveLEDs/DriveLEDsTest.cpp
+++ tests/DriveLEDs/DriveLEDsTest.cpp
@@ -4,15 +4,36 @@ extern "C" {
 #include "DriveLEDs.h"
 }
 
+/* ** Test List **
+ * All LEDs turned off on initialization
+ * Single LED turned on
+ * Single LED turned off
+ * Multiple LEDs turned on/off
+ * Turn on all LEDs
+ * Turn off all LEDs
+ * Query LED state
+ * Give LED color
+ * **
+*/
 
 TEST_GROUP(DriveLEDs)
 {
+	uint16_t virtualLEDStrip;
 	void setup()
 	{
+		DriveLEDs_Create(&virtualLEDStrip);
 	}
 
 	void teardown()
 	{
+		DriveLEDs_Destroy();
 	}
 
 };
+
+TEST(DriveLEDs, LEDsOffAfterInitialization)
+{
+	virtualLEDStrip = 0xffff;
+	DriveLEDs_Create(&virtualLEDStrip);
+	BYTES_EQUAL(0, virtualLEDStrip);
+}
