688fdf8ccb064a7ea4516684a0011ad39cff8ee4
Ronnie Nigash <nigash@uw.edu>
2017-02-02 08:56:31 -0800

    Begin protype of function call for colors

--- src/GatherColors/GatherColors.c
+++ src/GatherColors/GatherColors.c
@@ -1,3 +1,7 @@
+#include "GatherColors.h"
+/*int GatherColors() {
+	return system("/usr/bin/osascript GetPixelColor.applescript");
+}*/
 int main(int argc, char** argv){
 	system("/usr/bin/osascript GetPixelColor.applescript");
 	return 1;
