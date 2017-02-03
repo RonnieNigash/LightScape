f68303a73ce55c679b6ecc20dfcaba4a0592fe9f
Ronnie Nigash <nigash@uw.edu>
2017-02-03 10:21:07 -0800

    Update C wrapper of Python

--- src/GatherColors/GatherColors.c
+++ src/GatherColors/GatherColors.c
@@ -1,8 +1,25 @@
+#include <stdio.h>
 #include "GatherColors.h"
-/*int GatherColors() {
-	return system("/usr/bin/osascript GetPixelColor.applescript");
-}*/
-int main(int argc, char** argv){
-	system("/usr/bin/osascript GetPixelColor.applescript");
-	return 1;
+
+int GatherColors() {
+	char *command = "python GatherColorsPy.py";
+
+	char buffer[128];
+	FILE *fd;
+
+	if (( fd = popen(command, "r")) == NULL ) {
+		printf("Error opening pipe!\n");
+		return -1;
+	}
+
+	while (fgets(buffer, 128, fd) != NULL ) {
+		printf("Output:\t%s", buffer);
+	}
+
+	if ( pclose(fd) ) {
+		printf( "Command not found or exited with error status\n");
+		return -1;
+	}
+
+	return 0;
 }
