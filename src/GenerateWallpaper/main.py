32defe15c5b48078e2fdb8d3c4fb616fa295b00b
Ronnie Nigash <nigash@uw.edu>
2017-02-10 17:25:33 -0800

    Add generate_structure method to create shapes

--- src/GenerateWallpaper/main.py
+++ src/GenerateWallpaper/main.py
@@ -3,13 +3,27 @@
 
 from os import path
 from PIL import Image, ImageDraw
+from math import ceil
 import random
 
 def generate_GRB():
     return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
+def generate_structure(num_shapes_horizontal, num_shapes_vertical, shape_size, wallpaper_size):
+    points = []
+    for x in range(0, num_shapes_horizontal):
+        for y in range(0, num_shapes_vertical):
+            coordinates = [
+                    ( (x) * shape_size[0],      (y) * shape_size[1]),
+                    ( (x+1) * shape_size[0],    (y) * shape_size[1]),
+                    ( (x) * shape_size[0],      (y+1) * shape_size[1]),
+                    ( (x+1) * shape_size[0],    (y+1) * shape_size[1])
+            ]
+            points.append(coordinates)
+    return points 
+
 resolution = [1680, 1050]
-size_shapes = (120, 100)
+shape_size = (120, 100)
 
 file_directory = path.realpath(__file__).rstrip("/main.py")
 
@@ -25,8 +39,7 @@ colors = []
 for i in range(0, 2):
     colors.append(generate_GRB())
 
-number_shapes = (resolution[0] / size_shapes[0])
-number_shapes = (resolution[1] / size_shapes[1])
+num_shapes_horizontal = ceil((resolution[0] / shape_size[0]))
+num_shapes_vertical = ceil((resolution[1] / shape_size[1]))
 
-
-
+structure_of_shapes = generate_structure(num_shapes_horizontal, num_shapes_vertical, shape_size, wallpaper.size)
