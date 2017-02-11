c45927dab08a2f49e59a63c2886a44963b4a65ec
Ronnie Nigash <nigash@uw.edu>
2017-02-10 16:12:55 -0800

    Add image creation

--- src/GenerateWallpaper/main.py
+++ src/GenerateWallpaper/main.py
@@ -1,7 +1,18 @@
-#!/usr/bin/python3
 
-def main:
+#!/usr/local/bin/python3
 
+from os import path
+from PIL import Image, ImageDraw
 
-if __name__ == "main":
-    main()
+resolution = [1680, 1050]
+
+file_directory = path.realpath(__file__).rstrip("/main.py")
+
+print(file_directory)
+
+output_file = "background.png"
+
+wallpaper = Image.new("RGB", resolution, 0)
+fill_canvas = ImageDraw.Draw(wallpaper)
+
+wallpaper.save(output_file) 
