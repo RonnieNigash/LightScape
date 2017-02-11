29c5c845e7bcee635bcbf2ffc647bfdfeaf5faae
Ronnie Nigash <nigash@uw.edu>
2017-02-10 16:27:12 -0800

    Add generate_GRB() method

--- src/GenerateWallpaper/main.py
+++ src/GenerateWallpaper/main.py
@@ -3,12 +3,14 @@
 
 from os import path
 from PIL import Image, ImageDraw
+import random
+
+def generate_GRB():
+    return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 resolution = [1680, 1050]
 
 file_directory = path.realpath(__file__).rstrip("/main.py")
-
-print(file_directory)
 
 output_file = "background.png"
 
@@ -16,3 +18,5 @@ wallpaper = Image.new("RGB", resolution, 0)
 fill_canvas = ImageDraw.Draw(wallpaper)
 
 wallpaper.save(output_file) 
+
+print(generate_GRB())
