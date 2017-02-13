d0d8c85fbca175141f1a873a1bb93f43eca1b469
Ronnie Nigash <nigash@uw.edu>
2017-02-12 18:12:32 -0800

    Restructure to split into modules

--- src/GenerateWallpaper/main.py
+++ src/GenerateWallpaper/main.py
@@ -5,25 +5,13 @@ from os import path
 from PIL import Image, ImageDraw
 from math import ceil
 import random
+import structure
 
 RESOLUTION = [1680, 1050]
 SHAPE_SIZE = (120, 100)
 
 def generate_GRB():
     return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-
-def generate_structure(num_shapes_horizontal, num_shapes_vertical, SHAPE_SIZE, wallpaper_size):
-    points = []
-    for x in range(0, num_shapes_horizontal):
-        for y in range(0, num_shapes_vertical):
-            coordinates = [
-                    ( (x) * SHAPE_SIZE[0],      (y) * SHAPE_SIZE[1]),
-                    ( (x+1) * SHAPE_SIZE[0],    (y) * SHAPE_SIZE[1]),
-                    ( (x) * SHAPE_SIZE[0],      (y+1) * SHAPE_SIZE[1]),
-                    ( (x+1) * SHAPE_SIZE[0],    (y+1) * SHAPE_SIZE[1])
-            ]
-            points.append(coordinates)
-    return points 
 
 file_directory = path.realpath(__file__).rstrip("/main.py")
 
@@ -41,8 +29,13 @@ for i in range(0, 2):
 num_shapes_horizontal = ceil((RESOLUTION[0] / SHAPE_SIZE[0]))
 num_shapes_vertical = ceil((RESOLUTION[1] / SHAPE_SIZE[1]))
 
-structure_of_shapes = generate_structure(num_shapes_horizontal, num_shapes_vertical, SHAPE_SIZE, wallpaper.size)
+structure_of_shapes = Structure(
+        wallpaper.size,
+        (num_shapes_horizontal, num_shapes_vertical),
+        SHAPE_SIZE
+        )
+structure_of_shapes.init()
 
-print(structure_of_shapes)
+generate_structure(num_shapes_horizontal, num_shapes_vertical, SHAPE_SIZE, wallpaper.size)
 
 wallpaper.save(output_file) 
