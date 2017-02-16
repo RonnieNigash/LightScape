19f6385ed4f81c66e6f15fe4d55238824a46d2d9
Ronnie Nigash <nigash@uw.edu>
2017-02-15 18:05:26 -0800

    Generate 10 images per run

--- src/GenerateWallpaper/main.py
+++ src/GenerateWallpaper/main.py
@@ -9,36 +9,38 @@ import random
 from structure import Structure
 
 RESOLUTION = [1680, 1050]
-SHAPE_SIZE = (60, 50)
+SHAPE_SIZE = (15, 13)
 
 def generate_GRB():
     return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 file_directory = path.realpath(__file__).rstrip("/main.py")
 
-output_file = "background.png"
+for run in range(20, 30):
+    output_file = "background" + str(run) + ".png"
 
-wallpaper = Image.new("RGB", RESOLUTION, 0)
-fill_canvas = ImageDraw.Draw(wallpaper)
+    wallpaper = Image.new("RGB", RESOLUTION, 0)
+    fill_canvas = ImageDraw.Draw(wallpaper)
 
 
-colors = []
+    colors = []
 
-for i in range(0, 2):
-    colors.append(generate_GRB())
+    for i in range(0, 2):
+        colors.append(generate_GRB())
 
-num_shapes_horizontal = ceil((RESOLUTION[0] / SHAPE_SIZE[0]))
-num_shapes_vertical = ceil((RESOLUTION[1] / SHAPE_SIZE[1]))
+    num_shapes_horizontal = ceil((RESOLUTION[0] / SHAPE_SIZE[0]))
+    num_shapes_vertical = ceil((RESOLUTION[1] / SHAPE_SIZE[1]))
 
-structure_of_shapes = Structure(
-        wallpaper.size,
-        (num_shapes_horizontal, num_shapes_vertical),
-        SHAPE_SIZE
-        )
-structure_of_shapes.generate_struct()
+    structure_of_shapes = Structure(
+            wallpaper.size,
+            (num_shapes_horizontal, num_shapes_vertical),
+            SHAPE_SIZE
+            )
+    structure_of_shapes.generate_struct()
 
-structure_of_shapes.populate_colors(colors[0], colors[1])
+    structure_of_shapes.populate_colors(colors[0], colors[1])
 
-structure_of_shapes.paint(fill_canvas)
+    structure_of_shapes.paint(fill_canvas)
 
-wallpaper.save(output_file) 
+    wallpaper.save(output_file) 
+
