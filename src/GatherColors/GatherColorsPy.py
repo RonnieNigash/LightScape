cbb09c4ace3806b10b745ebaac89a82b82266d87
Ronnie Nigash <nigash@uw.edu>
2017-02-16 21:57:00 -0800

    Remove dependency on AppleScript in GatherColorsPy

--- src/GatherColors/GatherColorsPy.py
+++ src/GatherColors/GatherColorsPy.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
-
+from PIL import ImageGrab
+"""
 apple_script_gather_colors = '''
     set {green, red, blue} to {-1, -1, -1}
 
@@ -30,3 +31,6 @@ apple_script_gather_colors = '''
 process = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate(apple_script_gather_colors)
 print ( process.returncode, stdout, stderr)
+"""
+screen_grab = ImageGrab.grab( bbox = (10, 10, 11, 11) )
+print ( screen_grab.getpixel( (0, 0) ))
