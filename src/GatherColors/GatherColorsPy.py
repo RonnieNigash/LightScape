ada44eabfb138d682e1d0cc8a99bd5a1689374ee
Ronnie Nigash <nigash@uw.edu>
2017-03-06 15:02:41 -0800

    Add looping across screen to gather colors

--- src/GatherColors/GatherColorsPy.py
+++ src/GatherColors/GatherColorsPy.py
@@ -1,36 +1,20 @@
-from subprocess import Popen, PIPE
 from PIL import ImageGrab
-"""
-apple_script_gather_colors = '''
-    set {green, red, blue} to {-1, -1, -1}
 
-    tell application "Digital Color Meter" to activate
-    tell application "System Events"
-	tell process "Digital Color Meter"
-		click menu item "Copy Color as Text" of menu 1 of menu bar item "Color" of menu bar 1
-	end tell
-    end tell
+SCREEN_RESOLUTION = [1680, 1050]
+NUM_X_LEDS = 2
+NUM_Y_LEDS = 2
 
-    delay 0.1
+width = SCREEN_RESOLUTION[0]
+height = SCREEN_RESOLUTION[1]
 
-    set grbColors to words of (the clipboard)
-    if (count of grbColors) is 3 then
-	set {green, red, blue} to {item 2 of grbColors, item 1 of grbColors, item 3 of grbColors}
-    end if
+num_x_steps = (int) (width / NUM_X_LEDS)
+num_y_steps = (int) (height / NUM_Y_LEDS)
 
-    delay 0.1
+screen_grab = []
 
-    tell application "System Events"
-	tell process "Digital Color Meter"
-		click menu item "Quit Digital Color Meter" of menu 1 of menu bar item "Digital Color Meter" of menu bar 1
-	end tell
-    end tell
+for x in range(0, width, num_x_steps):
+    for y in range(0, height, num_y_steps):
+        screen_grab.append(ImageGrab.grab( bbox = (x, y, x+1, y+1) ))
 
-    return {green, red, blue}'''
-
-process = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-stdout, stderr = process.communicate(apple_script_gather_colors)
-print ( process.returncode, stdout, stderr)
-"""
-screen_grab = ImageGrab.grab( bbox = (10, 10, 11, 11) )
-print ( screen_grab.getpixel( (0, 0) ))
+print ( screen_grab[0].getpixel( (0,0) ))
+print ( screen_grab[1].getpixel( (0,0) ))
